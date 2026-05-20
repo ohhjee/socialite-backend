c1ca25604b5ea166333d1fb0c76d86309b6fdb24
ohhjee <orotunjigbega@gmail.com>
2026-05-20 14:45:34 +0100

    God Please

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,8 +1,7 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 import { emailPass, emailUser } from "@/constant";
 import nodemailer from "nodemailer";
-// import dns from "dns";
-
-// dns.setDefaultResultOrder("ipv4first");
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
