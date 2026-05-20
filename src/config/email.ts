f6927301440e115e354cc44ad36c4b6289b3fa97
ohhjee <orotunjigbega@gmail.com>
2026-05-20 14:27:06 +0100

    God Please

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,19 +1,12 @@
 import { emailPass, emailUser } from "@/constant";
 import nodemailer from "nodemailer";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
 import dns from "dns";
 
 dns.setDefaultResultOrder("ipv4first");
-
-const transportOptions: SMTPTransport.Options = {
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
     user: emailUser,
     pass: emailPass,
   },
-};
-
-export const transporter = nodemailer.createTransport(transportOptions);
+});
