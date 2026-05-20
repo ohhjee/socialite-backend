b7134b8ae81bd0f0a09e5b6a1292879c0c97cdf9
ohhjee <orotunjigbega@gmail.com>
2026-05-20 14:56:17 +0100

    God Please

--- src/config/email.ts
+++ src/config/email.ts
@@ -3,9 +3,12 @@ dns.setDefaultResultOrder("ipv4first");
 import { emailPass, emailUser } from "@/constant";
 import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // ✅ force IPv4
   auth: {
     user: emailUser,
     pass: emailPass,
   },
-});
+} as any);
