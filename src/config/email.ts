191002df59db7783e234446f775d21d958ac3b1a
ohhjee <orotunjigbega@gmail.com>
2026-05-20 08:24:02 +0100

    God i don come

--- src/config/email.ts
+++ src/config/email.ts
@@ -2,7 +2,9 @@ import { emailPass, emailUser } from "@/constant";
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587, // ✅ use this
+  secure: false, // ✅ MUST be false
   auth: {
     user: emailUser,
     pass: emailPass,
